9c0afbf3e102da82ed08723dee49734c987db56a
xb0n3 <102184534+xb0n3@users.noreply.github.com>
2022-06-17 17:39:56 +0200

    Inputfield commands print in screen

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 let userCommand;
+let consoleScreen = document.querySelector('.screen');
 let inputButton = document.querySelector('.promptButton');
 let inputField = document.querySelector('.promptField[type="text"]');
 let inputValue = document.querySelector('form.input').addEventListener('submit', (userCommand) => {
   userCommand.preventDefault();
   userCommand = inputField.value;
   console.log(userCommand);
+  printCommand(userCommand);
   inputField.value = '';
 });
+
+function printCommand(userCommand) {
+  consoleScreen.innerHTML +=`<p>${userCommand}</p>`;
+};
