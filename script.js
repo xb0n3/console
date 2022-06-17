2638ef541ddcecb1192e009b85aeec8b45ff7d0f
xb0n3 <102184534+xb0n3@users.noreply.github.com>
2022-06-17 17:31:46 +0200

    Programmed inputfield

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert('JavaScript active');
+let userCommand;
+let inputButton = document.querySelector('.promptButton');
+let inputField = document.querySelector('.promptField[type="text"]');
+let inputValue = document.querySelector('form.input').addEventListener('submit', (userCommand) => {
+  userCommand.preventDefault();
+  userCommand = inputField.value;
+  console.log(userCommand);
+  inputField.value = '';
+});
