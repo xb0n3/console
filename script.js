4ccca404674ba7d9c208432b662d2b6dba8211f2
xb0n3 <102184534+xb0n3@users.noreply.github.com>
2022-06-18 13:43:24 +0200

    Implemented note mode

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
 let userCommand;
+let calcModeSwitch = false;
+let noteModeSwitch = false;
+let gameModeSwitch = false;
+let calcHelp = `The following commands are available in calc mode:<br>
+!reset (clear the console)<br>
+!help (display this message)<br>
+!exit (exit to menu mode)`;
+let noteHelp = `The following commands are available in note mode:<br>
+!print (print note documet)<br>
+!delete (delete the last word)<br>
+!clear (reset note document)<br>
+!reset (clear the console)<br>
+!help (display this message)<br>
+!exit (exit to menu mode)`;
+let consoleHelp = `The following commands are available:<br>
+!calc (enter calc mode)<br>
+!note (enter note mode)<br>
+!game (enter game mode)<br>
+!exit (to exit the above modes)<br>
+!reset (clear the console)<br>
+!user (to change user)<br>
+!help (display this message)`;
 let noteLog = '';
+let notePosition = '';
 let consoleScreen = document.querySelector('.screen');
 let inputButton = document.querySelector('.promptButton');
 let inputField = document.querySelector('.promptField[type="text"]');
 let inputValue = document.querySelector('form.input').addEventListener('submit', userCommand => {
   userCommand.preventDefault();
   userCommand = inputField.value;
-  checkCommand(userCommand);
+  if (calcModeSwitch == true) {
+    calcMode(userCommand);
+  } else if (noteModeSwitch == true) {
+    noteMode(userCommand);
+  } else if (gameModeSwitch == true) {
+    gameMode(userCommand);
+  } else { checkCommand(userCommand); }
   console.log(userCommand);
   focusScreen();
-//inputField.value = '';
 });
 
 function checkCommand(userCommand) {
@@ -21,24 +49,23 @@ function checkCommand(userCommand) {
       break;
     case (userCommand == '!calc'):
       printCommand('> Entered calc mode');
-      calcMode(userCommand);
+      calcModeSwitch = true;
       break;
     case (userCommand == '!note'):
       printCommand('> Entered note mode');
-      inputValue = document.querySelector('form.input').addEventListener('submit', userCommand => {
-        userCommand.preventDefault();
-        userCommand = inputField.value;
-        noteMode(userCommand);
-        console.log(userCommand);
-        focusScreen();
-      });
+      noteModeSwitch = true;
       break;
     case (userCommand == '!game'):
-      printCommand('> Entered game mode');
       gameMode(userCommand);
       break;
+    case (userCommand == '!help'):
+      printCommand(consoleHelp);
+      break;
+    case (userCommand == '!reset'):
+      resetScreen();
+      break;
     default:
-      return;
+      printCommand('Invalid command')
       break;
   }
 };
@@ -49,35 +76,49 @@ function printCommand(userCommand) {
 
 function focusScreen() {
   consoleScreen.scrollTop = consoleScreen.scrollHeight;
+  inputField.value = '';
 };
 
 function calcMode(userCommand) {
-  alert('calcMode');
-}
+  switch(true) {
+    case (userCommand == '!exit'):
+      noteModeSwitch = false;
+      printCommand('> Entered menu mode');
+      break;
+  }
+};
 
 function noteMode(userCommand) {
   switch(true) {
-    case (userCommand == '' ||
+    case (userCommand == '!note' || 
+          userCommand == '' || 
           userCommand == null || 
           userCommand == undefined):
+      return;
       break;
     case (userCommand == '!print'):
-      alert('!print');
+      printCommand(noteLog);
       break;
-    case (userCommand == '!delete'):
-      alert('!delete');
+    case (userCommand == '!delete'): // needs fixing
+      do {
+        noteLog.charAt(--notePosition);
+      } while (noteLog.charAt(notePosition) != ' ');
+      noteLog = noteLog.substring(0, notePosition);
+      printCommand(noteLog);
       break;
     case (userCommand == '!clear'):
-      alert('!clear');
+      printCommand('Document has been cleared')
+      noteLog = '';
       break;
     case (userCommand == '!reset'):
-      alert('!reset');
+      resetScreen();
       break;
     case (userCommand == '!help'):
-      alert('!help');
+      printCommand(noteHelp);
       break;
     case (userCommand == '!exit'):
-      alert('!exit');
+      noteModeSwitch = false;
+      printCommand('> Entered menu mode');
       break;
     default:
       if (noteLog == '') {
@@ -92,4 +133,9 @@ function noteMode(userCommand) {
 
 function gameMode(userCommand) {
   printCommand('> Game mode is currently unavailable');
-}
+  printCommand('> Entered menu mode');
+};
+
+function resetScreen() {
+  consoleScreen.innerHTML = '';
+};
